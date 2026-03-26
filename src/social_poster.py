7c6baaa2c7177328f7f2d5574719b503e71677dd
aravroshith1-prog <aravroshith1@gmail.com>
2026-03-26 20:09:39 +0530

    Create src/social_poster.py with SocialMediaPoster class

---
+++ src/social_poster.py
@@ -0,0 +1,5 @@
+class SocialMediaPoster:
+    def post_all(self, video_path, caption):
+        # TODO: Implement posting to TikTok, Instagram, YouTube Shorts
+        print(f"Posting {video_path} with caption '{caption}' to all socials (stub)")
+        return True
